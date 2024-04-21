ba5a106d96426509da03a680e02ac7f6781ed5b8
willymateo <wjmateo@espol.edu.ec>
2024-04-21 18:05:24 -0500

    Add routes to get warehouse by id and edit warehouse

--- src/middlewares/validateAJVSchema/warehouses.js
+++ src/middlewares/validateAJVSchema/warehouses.js
@@ -1,9 +1,22 @@
-const { createWarehouseSchema, getWarehousesSchema } = require("../../ajvSchemas/warehouses");
+const {
+  createWarehouseSchema,
+  getWarehousesSchema,
+  editWarehouseSchema,
+} = require("../../ajvSchemas/warehouses");
 const { validateAJVSchema } = require(".");
 
 const validateCreateWarehouseSchema = (req, res, next) => {
   req.ajv = {
     schema: createWarehouseSchema,
+    data: req.body,
+  };
+
+  validateAJVSchema(req, res, next);
+};
+
+const validateEditWarehouseSchema = (req, res, next) => {
+  req.ajv = {
+    schema: editWarehouseSchema,
     data: req.body,
   };
 
@@ -19,4 +32,8 @@ const validateGetWarehousesSchema = (req, res, next) => {
   validateAJVSchema(req, res, next);
 };
 
-module.exports = { validateCreateWarehouseSchema, validateGetWarehousesSchema };
+module.exports = {
+  validateCreateWarehouseSchema,
+  validateGetWarehousesSchema,
+  validateEditWarehouseSchema,
+};
